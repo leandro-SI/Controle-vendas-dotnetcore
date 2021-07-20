2b7c179bbd2baf2d1ea6bc817f65c2fa5ba69dc1
Leandro-SI <llmanutencao@bol.com.br>
2021-07-20 14:10:54 -0300

    estidade Cidade

---
+++ aspnet-core/src/ControleVendas.Core/Entities/Cidade.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ControleVendas.Entities
+{
+    public class Cidade : AuditedEntity<Guid>
+    {
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        [ForeignKey(nameof(EstadoId))]
+        public Estado Estado { get; set; }
+
+        [Required]
+        public Guid? EstadoId { get; set; }
+
+        public void Atualizar(string nome, Guid? estadoId)
+        {
+            Nome = nome;
+            EstadoId = estadoId;
+        }
+
+    }
+}
