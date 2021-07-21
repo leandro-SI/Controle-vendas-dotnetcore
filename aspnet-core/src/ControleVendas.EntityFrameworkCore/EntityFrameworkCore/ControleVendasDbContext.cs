16e9307fda556c0d849ae6800603846232f09b03
Leandro-SI <llmanutencao@bol.com.br>
2021-07-21 09:56:48 -0300

    migration Produto

--- aspnet-core/src/ControleVendas.EntityFrameworkCore/EntityFrameworkCore/ControleVendasDbContext.cs
+++ aspnet-core/src/ControleVendas.EntityFrameworkCore/EntityFrameworkCore/ControleVendasDbContext.cs
@@ -17,6 +17,7 @@ namespace ControleVendas.EntityFrameworkCore
         public DbSet<Fabricante> Fabricante { get; set; }
         public DbSet<Unidade> Unidade { get; set; }
         public DbSet<Tipo> Tipo { get; set; }
+        public DbSet<Produto> Produto { get; set; }
 
         public ControleVendasDbContext(DbContextOptions<ControleVendasDbContext> options)
             : base(options)
---
+++ aspnet-core/src/ControleVendas.EntityFrameworkCore/Migrations/20210721125624_CREATE_Produto.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ControleVendas.Migrations
+{
+    public partial class CREATE_Produto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierUserId = table.Column<long>(nullable: true),
+                    Descricao = table.Column<string>(maxLength: 150, nullable: true),
+                    Estoque = table.Column<decimal>(nullable: false),
+                    PrecoCusto = table.Column<decimal>(nullable: false),
+                    PrecoVenda = table.Column<decimal>(nullable: false),
+                    FabricanteId = table.Column<Guid>(nullable: false),
+                    UnidadeId = table.Column<Guid>(nullable: false),
+                    TipoId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produto_Fabricante_FabricanteId",
+                        column: x => x.FabricanteId,
+                        principalTable: "Fabricante",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Produto_Tipo_TipoId",
+                        column: x => x.TipoId,
+                        principalTable: "Tipo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Produto_Unidade_UnidadeId",
+                        column: x => x.UnidadeId,
+                        principalTable: "Unidade",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_FabricanteId",
+                table: "Produto",
+                column: "FabricanteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_TipoId",
+                table: "Produto",
+                column: "TipoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_UnidadeId",
+                table: "Produto",
+                column: "UnidadeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
