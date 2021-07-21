d1c71941c0ef3e3889cc4f34b177ce1648f6bb4a
Leandro-SI <llmanutencao@bol.com.br>
2021-07-21 09:32:53 -0300

    entidade Unidade

---
+++ aspnet-core/src/ControleVendas.Core/Entities/Unidade.cs
@@ -0,0 +1,25 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ControleVendas.Entities
+{
+    public class Unidade : AuditedEntity<Guid>
+    {
+        [Required]
+        [StringLength(20)]
+        public string Sigla { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        public void Atualizar(string sigla, string nome)
+        {
+            Sigla = sigla;
+            Nome = nome;
+        }
+    }
+}
