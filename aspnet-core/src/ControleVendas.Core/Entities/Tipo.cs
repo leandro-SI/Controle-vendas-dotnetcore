6ed55bb5c674eafc14b0acf0ecddc6866b311d36
Leandro-SI <llmanutencao@bol.com.br>
2021-07-21 09:38:11 -0300

    entidade Tipo

---
+++ aspnet-core/src/ControleVendas.Core/Entities/Tipo.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ControleVendas.Entities
+{
+    public class Tipo : AuditedEntity<Guid>
+    {
+        public Tipo()
+        {
+        }
+
+        public Tipo(string nome)
+        {
+            Nome = nome;
+        }
+
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        public void Atualizar(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
