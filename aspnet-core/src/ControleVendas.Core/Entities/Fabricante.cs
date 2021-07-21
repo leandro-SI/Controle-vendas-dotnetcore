ea8a151ab9394eb9c07df6f99745fb5a9da806df
Leandro-SI <llmanutencao@bol.com.br>
2021-07-21 09:24:47 -0300

    entidade fabricante

---
+++ aspnet-core/src/ControleVendas.Core/Entities/Fabricante.cs
@@ -0,0 +1,25 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ControleVendas.Entities
+{
+    public class Fabricante : AuditedEntity<Guid>
+    {
+        [Required]
+        [StringLength(50)]
+        public string Nome{ get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Site { get; set; }
+
+        public void Atualizar(string nome, string site)
+        {
+            Nome = nome;
+            Site = site;
+        }
+    }
+}
