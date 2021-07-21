f2f8596dd5d4212e3d351f0d9011fb115413bbc2
Leandro-SI <llmanutencao@bol.com.br>
2021-07-21 09:20:55 -0300

    entidade grupo

---
+++ aspnet-core/src/ControleVendas.Core/Entities/Grupo.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleVendas.Entities
+{
+    public class Grupo : AuditedEntity<Guid>
+    {
+
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        public void Atualizar(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
