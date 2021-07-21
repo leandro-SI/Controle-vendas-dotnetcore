faed16cbe286ba3f66c493691a06dd1261d2f44f
Leandro-SI <llmanutencao@bol.com.br>
2021-07-21 10:34:21 -0300

    entidade PessoaGrupo

---
+++ aspnet-core/src/ControleVendas.Core/Entities/PessoaGrupo.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ControleVendas.Entities
+{
+    public class PessoaGrupo : AuditedEntity<Guid>
+    {
+        public PessoaGrupo()
+        {
+        }
+
+        public PessoaGrupo(Guid? pessoaId, Guid? grupoId)
+        {
+            PessoaId = pessoaId;
+            GrupoId = grupoId;
+        }
+
+        [ForeignKey(nameof(PessoaId))]
+        public Pessoa Pessoa { get; set; }
+
+        [Required]
+        public Guid? PessoaId { get; set; }
+
+        [ForeignKey(nameof(GrupoId))]
+        public Grupo Grupo { get; set; }
+        [Required]
+        public Guid? GrupoId { get; set; }
+    }
+}
