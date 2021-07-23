afd0fe8ef42aecf5955aa51de19e23d9e2193893
Leandro-SI <llmanutencao@bol.com.br>
2021-07-23 15:06:51 -0300

    Dto para Cidade

---
+++ aspnet-core/src/ControleVendas.Application/ControleVendasApp/Cidade/Dto/CidadeDto.cs
@@ -0,0 +1,27 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using ControleVendas.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ControleVendas.ControleVendasApp.Cidade.Dto
+{
+    [AutoMap(typeof(Entities.Cidade))]
+    public class CidadeDto : AuditedEntityDto<Guid>
+    {
+        public CidadeDto()
+        {
+        }
+
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        public Estado Estado { get; set; }
+
+        [Required]
+        public Guid? EstadoId { get; set; }
+    }
+}
---
+++ aspnet-core/src/ControleVendas.Application/ControleVendasApp/Cidade/Dto/CreateCidadeDto.cs
@@ -0,0 +1,19 @@
+﻿using Abp.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ControleVendas.ControleVendasApp.Cidade.Dto
+{
+    [AutoMap(typeof(Entities.Cidade))]
+    public class CreateCidadeDto
+    {
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        [Required]
+        public Guid? EstadoId { get; set; }
+    }
+}
