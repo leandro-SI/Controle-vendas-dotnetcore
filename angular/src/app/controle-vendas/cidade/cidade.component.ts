01265aab12b560d1849d3905600f33c92166be18
Leandro-SI <llmanutencao@bol.com.br>
2021-07-23 17:00:58 -0300

    componente listagem de cidades

--- angular/src/app/controle-vendas/cidade/cidade.component.ts
+++ angular/src/app/controle-vendas/cidade/cidade.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { CidadeDto } from '@shared/models/cidade/cidadeDto';
+import { CidadeDtoPagedResultDto } from '@shared/models/cidade/cidadeDtoPagedResultDto';
+import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
+import { CidadeServiceProxy } from '@shared/service-proxies/service-proxies';
+import { finalize } from 'rxjs/internal/operators/finalize';
+
+class PagedCidadeRequestDto extends PagedRequestDto {
+  keyword: string;
+  status: number | undefined;
+}
 
 @Component({
   selector: 'app-cidade',
   templateUrl: './cidade.component.html',
-  styleUrls: ['./cidade.component.css']
+  styleUrls: ['./cidade.component.css'],
+  animations: [appModuleAnimation()]
 })
-export class CidadeComponent implements OnInit {
+export class CidadeComponent extends PagedListingComponentBase<CidadeDto> {  
+  advancedFiltersVisible: boolean = false;
+  keyword: string = '';
+  status: number | undefined;
+  cidades: CidadeDto[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(injector: Injector, private _router: Router, private cidadeService: CidadeServiceProxy) {
+    super(injector);
   }
 
+  clearFilters(): void {
+    this.keyword = '';
+    this.status = undefined;
+    this.getDataPage(1);
+  }
+
+  protected list(
+    request: PagedCidadeRequestDto,
+    pageNumber: number,
+    finishedCallback: Function
+  ): void {
+    request.keyword = this.keyword;
+    request.status = this.status;
+    this.cidadeService
+      .getAll(request.keyword, request.status, request.skipCount, request.maxResultCount )
+      .pipe(
+        finalize(() => {
+          finishedCallback();
+        })
+      )
+      .subscribe((result: CidadeDtoPagedResultDto) => {
+        this.cidades = result.items;
+        this.showPaging(result, pageNumber);
+      });
+  }
+
+  protected delete(cidade: CidadeDto): void {
+
+  }
+
+  public novo(){
+  }
+
+  public detalhe(id:any){
+  }
+
+  public editar(id:any){
+  }
+
+  public voltar(){
+    this._router.navigate(['/app/cidades/']);
+  }
 }
