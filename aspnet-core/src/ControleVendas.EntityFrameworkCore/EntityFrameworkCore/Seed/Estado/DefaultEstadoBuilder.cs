170d6964a174a2531f5b9c693f278e03840cdfe4
Leandro-SI <llmanutencao@bol.com.br>
2021-07-23 14:53:19 -0300

    Seed para adicionar Estados

--- aspnet-core/src/ControleVendas.EntityFrameworkCore/EntityFrameworkCore/Seed/Estado/DefaultEstadoBuilder.cs
+++ aspnet-core/src/ControleVendas.EntityFrameworkCore/EntityFrameworkCore/Seed/Estado/DefaultEstadoBuilder.cs
@@ -23,18 +23,55 @@ namespace ControleVendas.EntityFrameworkCore.Seed.Estado
 
         private void CreateDefaultEstados()
         {
-            var defaultEstado = _context.Estado.FirstOrDefault(t => t.Nome == "Brasil");
 
-            if (defaultEstado == null)
+            Dictionary<string, string> dictEstados = new Dictionary<string, string>();
+
+            dictEstados.Add("AC", "Acre");
+            dictEstados.Add("AL", "Alagoas");
+            dictEstados.Add("AP", "Amapá");
+            dictEstados.Add("AM", "Amazonas");
+            dictEstados.Add("BA", "Bahia");
+            dictEstados.Add("CE", "Ceará");
+            dictEstados.Add("ES", "Espírito Santo");
+            dictEstados.Add("GO", "Goiás");
+            dictEstados.Add("MA", "Maranhão");
+            dictEstados.Add("MT", "Mato Grosso");
+            dictEstados.Add("MS", "Mato Grosso do Sul");
+            dictEstados.Add("MG", "Minas Gerais");
+            dictEstados.Add("PA", "Pará");
+            dictEstados.Add("PB", "Paraíba");
+            dictEstados.Add("PR", "Paraná");
+            dictEstados.Add("PE", "Pernambuco");
+            dictEstados.Add("PI", "Piauí");
+            dictEstados.Add("RJ", "Rio de Janeiro");
+            dictEstados.Add("RN", "Rio Grande do Norte");
+            dictEstados.Add("RS", "Rio Grande do Sul");
+            dictEstados.Add("RO", "Rondônia");
+            dictEstados.Add("RR", "Roraima");
+            dictEstados.Add("SC", "Santa Catarina");
+            dictEstados.Add("SP", "São Paulo");
+            dictEstados.Add("SE", "Sergipe");
+            dictEstados.Add("TO", "Tocantins");
+            dictEstados.Add("DF", "Distrito Federal");
+
+            foreach (var est in dictEstados)
             {
-                defaultEstado = new Entities.Estado();
+                var defaultEstado = _context.Estado.FirstOrDefault(t => t.Nome == est.Value);
 
-                defaultEstado.Nome = "Brasil";
-                defaultEstado.Sigla = "BR";
+                if (defaultEstado == null)
+                {
+                    var estado = new Entities.Estado();
 
-                _context.Estado.Add(defaultEstado);
-                _context.SaveChanges();
+                    estado.Nome = est.Value;
+                    estado.Sigla = est.Key;
+
+                    _context.Estado.Add(estado);
+
+                }
             }
+
+            _context.SaveChanges();
+
         }
     }
 }
